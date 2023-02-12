58235784ff299a6c2dfac3b5f135a195de18f95e
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-02-12 20:52:19 +0300

    Initial commit

---
+++ src/test/java/edu/kocaeli/actofgod_api/ActOfGodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kocaeli.actofgod_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActOfGodApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
