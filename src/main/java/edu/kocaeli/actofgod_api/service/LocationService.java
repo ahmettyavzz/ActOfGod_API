c8e305981e28e44266101c7b00c9a324cac0e91e
ahmettyavzz <ahmethamdiyavuzz@gmail.com>
2023-04-09 01:42:40 +0300

    create LocationService class in service layer

---
+++ src/main/java/edu/kocaeli/actofgod_api/service/LocationService.java
@@ -0,0 +1,52 @@
+package edu.kocaeli.actofgod_api.service;
+
+import edu.kocaeli.actofgod_api.converter.LocationConverter;
+import edu.kocaeli.actofgod_api.dto.LocationDto;
+import edu.kocaeli.actofgod_api.exception.ErrorStatusCode;
+import edu.kocaeli.actofgod_api.exception.GeneralException;
+import edu.kocaeli.actofgod_api.model.Location;
+import edu.kocaeli.actofgod_api.repository.LocationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class LocationService {
+    private final LocationRepository locationRepository;
+    private final LocationConverter locationConverter;
+
+    public LocationDto add(LocationDto locationDto) {
+        var forSave = locationConverter.toEntity(locationDto);
+        forSave.setCreatedDate(new Date());
+        return locationConverter.toDto(locationRepository.save(forSave));
+    }
+
+    public LocationDto getById(Long id) {
+        var locationById = getLocationIfFound(id);
+        return locationConverter.toDto(locationById);
+    }
+
+    public List<LocationDto> getAll() {
+        return locationRepository.findAll().stream().map(locationConverter::toDto).collect(Collectors.toList());
+    }
+
+    public LocationDto update(Long id, LocationDto dto) {
+        var forUpdateLocation = getLocationIfFound(id);
+        forUpdateLocation.setName(dto.getName());
+        forUpdateLocation.setLatitude(dto.getLatitude());
+        forUpdateLocation.setLongitude(dto.getLongitude());
+        forUpdateLocation.setCapacity(forUpdateLocation.getCapacity());
+        forUpdateLocation.setUpdatedDate(new Date());
+        return locationConverter.toDto(locationRepository.save(forUpdateLocation));
+    }
+
+    private Location getLocationIfFound(Long id) {
+        return locationRepository.findById(id).orElseThrow(
+                () -> new GeneralException(ErrorStatusCode.LOCATION_NOT_FOUND_BY_ID)
+        );
+    }
+}
