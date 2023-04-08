2ee4df4342de639efc28d6d93d5a03924c9494dc
ahmettyavzz <ahmethamdiyavuzz@gmail.com>
2023-04-09 01:41:39 +0300

    create LocationConverter class in converter layer

---
+++ src/main/java/edu/kocaeli/actofgod_api/converter/LocationConverter.java
@@ -0,0 +1,22 @@
+package edu.kocaeli.actofgod_api.converter;
+
+import edu.kocaeli.actofgod_api.dto.LocationDto;
+import edu.kocaeli.actofgod_api.model.Location;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LocationConverter {
+    private final ModelMapper modelMapper;
+
+    public Location toEntity(LocationDto dto) {
+        return modelMapper.map(dto, Location.class);
+    }
+
+    public LocationDto toDto(Location entity) {
+        return modelMapper.map(entity, LocationDto.class);
+    }
+
+}
