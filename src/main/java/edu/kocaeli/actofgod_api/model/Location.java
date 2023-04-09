b87ced2daae13a02477eb8aa58a0b4db6edaaca3
ahmettyavzz <ahmethamdiyavuzz@gmail.com>
2023-04-09 17:23:51 +0300

    update Location class

--- src/main/java/edu/kocaeli/actofgod_api/model/Location.java
+++ src/main/java/edu/kocaeli/actofgod_api/model/Location.java
@@ -2,17 +2,23 @@ package edu.kocaeli.actofgod_api.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
 @Table(name = "locations")
-public class Location {
+public class Location extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -20,6 +26,4 @@ public class Location {
     private Double latitude;
     private Double longitude;
     private Long capacity;
-    private Date createdDate;
-    private Date updatedDate;
 }
