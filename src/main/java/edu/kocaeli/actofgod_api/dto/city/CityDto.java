a782dadd8edb32f45dc941068290d0548fcfea8c
coz <can.oz@32bit.com.tr>
2023-04-30 17:59:26 +0300

    city dtos added

---
+++ src/main/java/edu/kocaeli/actofgod_api/dto/city/CityDto.java
@@ -0,0 +1,13 @@
+package edu.kocaeli.actofgod_api.dto.city;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CityDto {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/edu/kocaeli/actofgod_api/dto/city/CreateCityDto.java
@@ -0,0 +1,12 @@
+package edu.kocaeli.actofgod_api.dto.city;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CreateCityDto {
+    private String name;
+}
